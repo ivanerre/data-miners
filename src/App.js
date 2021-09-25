1d0194bd453d3697e177ce548f245707cf16d4c2
Iván Juárez <ivanr.jm@gmail.com>
2021-09-25 15:55:36 -0500

    First version of the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Box, Heading } from "@chakra-ui/react"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Box bg="gray.700" color="white" w="100%" p={6}>
+        <Heading>Hot Pursuit Challenge</Heading>
+      </Box>
   );
 }
 
